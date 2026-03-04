bd8de23f410d22197d00e416703492bffc67cd30
haarish1999 <haarish1999@gmail.com>
2026-03-04 10:41:34 +0530

    Fix ESLint config for v8

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,15 +1,17 @@
-module.exports = [
-  {
-    files: ["**/*.js"],
-    languageOptions: {
-      ecmaVersion: 2022,
-      sourceType: "commonjs"
-    },
-    rules: {
-      "no-unused-vars": ["error"],
-      "no-console": ["warn"],
-      "semi": ["error", "always"],
-      "quotes": ["error", "double"]
-    }
-  }
-];
+module.exports = {
+  env: {
+    node: true,
+    jest: true,
+    es2022: true,
+  },
+  parserOptions: {
+    ecmaVersion: 2022,
+    sourceType: "commonjs",
+  },
+  rules: {
+    "no-unused-vars": "error",
+    "no-console": "warn",
+    "semi": ["error", "always"],
+    "quotes": ["error", "double"],
+  },
+};
